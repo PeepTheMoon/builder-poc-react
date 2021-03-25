e5e9ffcce33cfafe26c9cf29a59786b4db5033f7
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2021-03-25 12:59:44 -0700

    edited header to display content ecreated int he dashboard with success!  YESS

--- builder-poc-react/src/components/Header.jsx
+++ builder-poc-react/src/components/Header.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-import { builder } from '@builder.io/react';
+import { builder, BuilderComponent } from '@builder.io/react';
 
 builder.init('3c7e390ebd25467aa0bf62675258be66');
 
-export const Header = () => (
-  <>
+export function Header(props) {
+return <>
     <h1>This is the header with nav links</h1>
     <Router>
       <Link to="/">Home</Link>
       <br></br>
       <Link to="display-page">Display Page</Link>
     </Router>
-  {/* <BuilderComponent
+    <BuilderComponent
     model="header"
-  /> */}
+    contentLoaded={props.content}
+    />
   </>
-)
+}
+  
+
--- builder-poc-react/src/pages/DisplayPage.jsx
+++ builder-poc-react/src/pages/DisplayPage.jsx
@@ -9,6 +9,7 @@ export function DisplayPage(props) {
   return <>
   <h1>This is the display page</h1>
   <BuilderComponent
+    // In some places I see this key as content=, but the content for this page from the dashboard wasn't rendering.  I changed it below based on the codepen sent to me from the forum post!
     contentLoaded={props.content}
     model="display-page" />
     </>
