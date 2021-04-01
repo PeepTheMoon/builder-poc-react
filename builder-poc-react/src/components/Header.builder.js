ae402be4311f370b44e8aa5d78e1d112ddb4dccb
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2021-04-01 13:45:25 -0700

    more updates to header, props still not rendering as expected

--- builder-poc-react/src/components/Header.builder.js
+++ builder-poc-react/src/components/Header.builder.js
@@ -1,11 +1,9 @@
 import { Header } from './Header.jsx';
-import { Builder, withChildren } from '@builder.io/react';
+import { Builder } from '@builder.io/react';
 
-const BuilderHeader = withChildren(Header);
-
-Builder.registerComponent(BuilderHeader, {
-    name: 'header',
-    inputs: [
+export const HeaderBuilderConfig = {
+  name: 'header',
+  inputs: [
       { 
         name: 'backgroundColor', 
         type: 'color',
@@ -18,5 +16,26 @@ Builder.registerComponent(BuilderHeader, {
         defaultValue: 'This is the header'
       }
     ]
-  }
-);
+};
+
+Builder.registerComponent(Header, HeaderBuilderConfig);
+
+// const BuilderHeader = withChildren(Header);
+
+// Builder.registerComponent(BuilderHeader, {
+//     name: 'header',
+//     inputs: [
+//       { 
+//         name: 'backgroundColor', 
+//         type: 'color',
+//         required: true,
+//         defaultValue: '#B8E986' 
+//       }, {
+//         name: 'textContent',
+//         type: 'text',
+//         required: true,
+//         defaultValue: 'This is the header'
+//       }
+//     ]
+//   }
+// );
--- builder-poc-react/src/components/Header.jsx
+++ builder-poc-react/src/components/Header.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-import { builder, Builder } from '@builder.io/react';
-
-builder.init('3c7e390ebd25467aa0bf62675258be66');
 
 export function Header(props) {
   const { backgroundColor } = props;
 
   return <>
-    <section style={backgroundColor}>
+    <section style={{ backgroundColor: backgroundColor }}>
       <Router>
         <Link to="/">Home</Link>
           <br></br>
